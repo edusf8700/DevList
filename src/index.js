ffbb5837e76e3f2fbdc68a66aa5370478412e180
Eduardo Freitas <edusf8700@gmail.com>
2020-03-05 04:57:18 -0300

    Configurado rotas com React Navigation e configurado StatusBar e Styled Components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { StatusBar } from 'react-native';
 
 import './config/ReactotronConfig';
 
+import Routes from './routes';
+
 function App() {
-  return <Text>Edu</Text>;
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <Routes />
+    </>
+  );
 }
 
 export default App;
