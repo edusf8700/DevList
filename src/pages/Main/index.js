656e74dafa833f847b24555de40ff0ab43dbf1d6
Eduardo Freitas <edusf8700@gmail.com>
2020-03-10 04:03:02 -0300

    Estilizado formulário

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container } from './styles';
+import { Container, Input, Form, SubmitButton } from './styles';
 
 export default function Main() {
-  return <Container />;
+  return (
+    <Container>
+      <Form>
+        <Input
+          placeholder="Adicionar Usuários"
+          autoCorrect={false}
+          autoCapitalize="none"
+        />
+        <SubmitButton>
+          <Icon name="add" size={20} color="#fff" />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
 
 Main.navigationOptions = {
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,7 +1,34 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   flex: 1;
   padding: 30px;
-  background-color: #333;
+`;
+
+export const Form = styled.View`
+  flex-direction: row;
+  padding-bottom: 20px;
+  border-bottom-width: 1px;
+  border-color: #eee;
+`;
+
+export const Input = styled.TextInput.attrs({
+  placeholderTextColor: '#999',
+})`
+  flex: 1;
+  background: #eee;
+  padding: 0 15px;
+  height: 40px;
+  border-radius: 4px;
+  border: 1px solid #eee;
+`;
+
+export const SubmitButton = styled(RectButton)`
+  background: #7159c1;
+  justify-content: center;
+  align-items: center;
+  border-radius: 4px;
+  padding: 0 12px;
+  margin-left: 10px;
 `;
